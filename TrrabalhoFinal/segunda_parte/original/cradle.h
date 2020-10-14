6a5726779e01520baa04676cef69b690238c2390
DanyelleAngelo <danyangelofsa@gmail.com>
2020-10-14 08:31:28 -0300

    Backup cradle original

---
+++ TrrabalhoFinal/segunda_parte/original/cradle.h
@@ -0,0 +1,53 @@
+#ifndef CRADLE_H
+#define CRADLE_H
+
+char lookahead;
+
+
+/*
+*@brief inicia o compilador
+*/
+void init();
+
+/*
+*@brief lê caractere
+*/
+void nextChar();
+
+/*
+*@brief mostra uma mensagem de erro com formatação
+*/
+void error(char *s);
+
+/*
+*@brief mostra uma mensagem de erro e encerra o programa
+*/
+void fatal(char *s);
+
+/*
+*@brief verifica se a entrada dá match com o esperado, chamand expected c.c.
+*/
+void match(char c);
+
+
+/*
+*@brief verifca se o nome de um indetificador é formado por letras
+*/
+char getName();
+
+/*
+*@brief verifica se lookhead é um número
+*/
+char getNum();
+
+/*
+*@brief verifica se o caractere analisado é um operador de soma ou subtração
+*/
+int isAddOp(char c);
+
+/*
+*@brief verifica se o caractere analisado é um operador de multiplicação ou divisão
+*/
+int isMulOp(char c);
+
+#endif
---
+++ TrrabalhoFinal/segunda_parte/original/main.c
@@ -0,0 +1,9 @@
+#include <string.h>
+#include <stdio.h>
+#include "cradle.h"
+
+
+int main(){
+	init();
+	return 0;
+}
