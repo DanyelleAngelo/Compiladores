2c13c24c635cb68ded10dc1774bc68907a440d64
DanyelleAngelo <danyangelofsa@gmail.com>
2020-10-14 22:33:55 -0300

    Tiny 0.1!!! Capítul 10, estrturas de controle

--- TrrabalhoFinal/segunda_parte/Tiny/tiny.c
+++ TrrabalhoFinal/segunda_parte/Tiny/tiny.c
@@ -6,7 +6,122 @@
 
 #include "tiny.h"
 
+void asm_clear(){
+	printf("\txor ax, ax\n");
+}
+
+void asm_negative(){
+	printf("\tneg ax\n");
+}
+
+void asm_loadconst(int i){
+	printf("\tmov ax, %d\n", i);
+}
+
+void asm_loadvar(char name){
+	if(!intable(name))undefined(name);
+	printf("\tmov ax, word ptr %c\n", name);
+}
+
+void asm_push(){
+	printf("\tpush ax\n");
+}
+
+void asm_popadd(){
+	printf("\tpop bx\n");
+	printf("\tadd ax, bx\n");
+}
+
+void asm_popsub(){
+	printf("\tpop bx\n");
+	printf("\tsub ax, bx\n");
+	printf("\tneg ax\n");
+}
+
+void asm_popmul(){
+	printf("\tpop bx\n");
+	printf("\timul bx\n");
+}
+
+void asm_popdiv(){
+	printf("\tpop bx\n");
+	printf("\txchg ax, bx\n");
+	printf("\tcwd\n");
+	printf("\tidiv bx\n");
+}
+
+void asm_store(char name){
+	if(!intable(name))undefined(name);
+	printf("\tmov word ptr bx, ax\n");
+}
+
+void asm_not(){
+	printf("\tnot ax\n");
+}
+
+void asm_popand(){
+	printf("\tpop bx\n");
+	printf("\tand ax, bx\n");
+}
+
+void asm_popor(){
+	printf("\tpop bx\n");
+	printf("\tor ax, bx\n");	
+}
+
+void asm_popxor(){
+	printf("\tpop bx\n");
+	printf("\txor ax, bx\n");
+}
+
+void asm_popcompare(){
+	printf("\tpop bx\n");
+	printf("\tcmp bx, ax\n");	
+}
+
+void asm_relop(char op){
+	char *jump;
+	int l1,l2;
+	l1 = newLabel();
+	l2 = newLabel();
+
+	switch(op){
+		case '=':
+			jump = "je";
+			break;
+		case '#':
+			jump = "jne";
+			break;
+		case '<':
+			jump = "jl";
+			break;
+		case '>':
+			jump = "jg";
+			break;
+	}
+	printf("\t%s L%d\n",jump,l1);
+	printf("\txor ax, ax\n");
+	printf("\tjmp L%d\n",l2);
+	printf("L%d:\n",l1);
+	printf("\tmov ax, -1\n");
+	printf("L%d:\n",l2);
+}
+
+void asm_jmp(int label){
+	printf("\tjmp L%d\n", label);
+}
+
+void asm_jmpfalse(int label){
+	printf("\tjz L%d\n", label);
+}
+
+void asm_read(){}
+
+void asm_write(){}
+
 void init(){
+	labelCount = 0;
+	varTbl = calloc(VAR_TBL_SZ,sizeof(int));
 	nextChar();
 }
 
@@ -26,6 +141,10 @@ void fatal(char *s){
 void expected(char *s){
 	fprintf(stderr, "Error: %s expected\n",s);
 	exit(1);
+}
+
+int newLabel(){
+	return labelCount++;
 }
 
 void match(char c){
@@ -69,33 +188,41 @@ int isMulOp(char c){
 	return (c == '*' || c == '/');
 }
 
+int isOrOp(char c){
+	return (c == '|' || c == '~');
+}
+
+int isRelOp(char c){
+	return (strchr("=#<>",c)!=NULL);
+}
+
+int intable(char name){
+	return (varTbl[name - 'A'] != 0);
+}
+
+void undefined(char name){
+	fprintf(stderr, "Error: Undefinied identifier %c\n",name);
+	exit(1);
+}
+
 void allocVar(char name){
 	int value = 0, signal = 1;
-	if(lookahead == '='){
-		match('=');
-		if(lookahead == '-'){
-			match('-');
-			signal = -1;
+
+	if(intable(name)){
+		fprintf(stderr, "Duplicate variable name: %c\n", name);
+		exit(1);
+	}else{
+		varTbl[name - 'A'] = 1;
+		if(lookahead == '='){
+			match('=');
+			if(lookahead == '-'){
+				match('-');
+				signal = -1;
+			}
+			value = signal * getNum();
 		}
-		value = signal * getNum();
+		printf("%c:\tdw %d\n",name,value);
 	}
-	printf("%c:\tdw %d\n",name,value);
-}
-
-void prog(){
-	match('p');
-	header();
-	topDecls();
-	mainBlock();
-	match('.');
-}
-
-void header(){
-	printf(".model small\n");
-	printf(".stack\n");
-	printf(".code\n");
-	printf("PROG segment byte public\n");
-	printf("\tassume cs:PROG,ds:PROG,es:PROG,ss:PROG\n");
 }
 
 void topDecls(){
@@ -121,12 +248,255 @@ void decl(){
 	}
 }
 
+void block(){
+	int follow = 0;
+	while(!follow){
+		switch(lookahead){
+			case 'i':
+				doIf();
+				break;
+			case 'w':
+				doWhile();
+				break;
+			case 'e':
+			case 'l':
+				follow = 1;
+				break;
+			default:
+				assignment();
+				break;
+		}
+	}
+}
+
+void term1(){
+	while(isMulOp(lookahead)){
+		asm_push();
+		switch(lookahead){
+			case '*':
+				multiply();
+				break;
+			case '/':
+				divide();
+				break;
+		}
+	}
+}
+
+void term(){
+	factor();
+	term1();
+}
+
+void firstTerm(){
+	firstFactor();
+	term1();
+}
+
+void expression(){
+	firstTerm();
+	while(isAddOp(lookahead)){
+		asm_push();
+		switch(lookahead){
+			case '+':
+				add();
+				break;
+			case '-':
+				subtract();
+				break;
+		}
+	}
+}
+
+
+void factor(){
+	if(lookahead == '('){
+		match('(');
+		//expression();
+		boolExpression();
+		match(')');
+	}else if(isalpha(lookahead)){
+		asm_loadvar(getName());
+	}else{
+		asm_loadconst(getNum());
+	}
+}
+
+void negFactor(){
+	match('-');
+	if(isdigit(lookahead)){
+		asm_loadconst(-getNum());
+	}else{
+		factor();
+		asm_negative();
+	}
+}
+
+void firstFactor(){
+	switch(lookahead){
+		case '+':
+			match('+');
+			factor();
+			break;
+		case '-':
+			match('-');
+			factor();
+			break;
+		default:
+			factor();
+			break;
+	}
+}
+
+void assignment(){
+	char name = getName();
+	match('=');
+	//expression();
+	boolExpression();
+	asm_store(name);
+}
+
+void relation(){
+	char op;
+	expression();
+	if(isRelOp(lookahead)){
+		op = lookahead;
+		match(op);
+		asm_push();
+		expression();
+		asm_popcompare();
+		asm_relop(op);
+	}
+}
+
+void notFactor(){
+	if(lookahead == '!'){
+		match('!');
+		relation();
+		asm_not();
+	}else relation();
+}
+
+void boolTerm(){
+	notFactor();
+	while(lookahead == '&'){
+		asm_push();
+		match('&');
+		notFactor();
+		asm_popand();
+	}
+}
+
+void boolExpression(){
+	boolTerm();
+	while(isOrOp(lookahead)){
+		asm_push();
+		switch(lookahead){
+			case '|':
+				boolOr();
+				break;
+			case '~':
+				boolXor();
+				break;
+		}
+	}
+}
+
+void boolOr(){
+	match('|');
+	boolTerm();
+	asm_popor();
+}
+
+void boolXor(){
+	match('~');
+	boolTerm();
+	asm_popxor();
+}
+
+void add(){
+	match('+');
+	term();
+	asm_popadd();
+}
+
+void subtract(){
+	match('+');
+	term();
+	asm_popsub();
+}
+
+void multiply(){
+	match('*');
+	factor();
+	asm_popmul();
+}
+
+void divide(){
+	match('/');
+	factor();
+	asm_popdiv();
+}
+
+void doIf(){
+	int l1,l2;
+	match('i');
+	boolExpression();
+	l1 = newLabel();
+	l2 = l1;
+
+	asm_jmpfalse(l1);
+	block();
+	if(lookahead  == 'l'){
+		match('l');
+		l2 = newLabel();
+		asm_jmp(l2);
+		printf("L%d\n",l1);
+		block();
+	}
+	printf("L%d\n",l2);
+	match('e');
+}
+
+void doWhile(){
+	int l1,l2;
+	match('w');
+	l1 = newLabel();
+	l2 = newLabel();
+	printf("L%d\n",l1);
+	boolExpression();
+	asm_jmpfalse(l2);
+	block();
+	match('e');
+	asm_jmp(l1);
+	printf("L%d\n",l2);
+}
+
 void mainBlock(){
 	match('b');
 	prolog();
+	block();
 	match('e');
 	epilog();
 }
+
+void prog(){
+	match('p');
+	header();
+	topDecls();
+	mainBlock();
+	match('.');
+	free(varTbl);
+}
+
+void header(){
+	printf(".model small\n");
+	printf(".stack\n");
+	printf(".code\n");
+	printf("PROG segment byte public\n");
+	printf("\tassume cs:PROG,ds:PROG,es:PROG,ss:PROG\n");
+}
+
 
 void prolog(){
 	printf("MAIN:\n");
--- TrrabalhoFinal/segunda_parte/Tiny/tiny.h
+++ TrrabalhoFinal/segunda_parte/Tiny/tiny.h
@@ -1,11 +1,113 @@
 #ifndef TINY_H
 #define TINY_H
 
+#define VAR_TBL_SZ 26
 char lookahead;
+int *varTbl, labelCount; 
 
+/* 
+*@brief zera o registrador primário
+*/
+void asm_clear();
+
+/* 
+*@brief negativa o registrador primário
+*/
+void asm_negative();
+
+/* 
+*@brief carrega uma constante numérica no registrador primário
+*/
+void asm_loadconst(int i);
+
+/* 
+*@brief carrega uma variável no registrador primário
+*/
+void asm_loadvar(char name);
 
 /*
-*@brief inicia o compilador
+*@brief coloca registrador primário na pilha
+*/
+void asm_push();
+
+/*
+*@brief adiciona o topo da pilha ao registrador primário
+*/
+void asm_popadd();
+
+/*
+*@brief subtrai o registrador primário do topo da pilha
+*/
+void asm_popsub();
+
+/*
+*@brief multiplica o topo da pilha pelo registrador primário
+*/
+void asm_popmul();
+
+/*
+*@brief divide o topo da pilha pelo registrador primário
+*/
+void asm_popdiv();
+
+/*
+*@brief armazena registrador primário em variável
+*/
+void asm_store(char name);
+
+/*
+*@brief inverte registrador primário
+*/
+void asm_not();
+
+/*
+*@brief cálculo de "E" do topo da pilha com registrador primário
+*/
+void asm_popand();
+
+/*
+*@brief cálculo de "OU" do topo da pilha com registrador primário
+*/
+void asm_popor();
+
+/*
+*@brief cálculo de "OU-exclusivo" com o topo da pilha com registrador primário
+*/
+void asm_popxor();
+
+/*
+*@brief compara o topo da pilha com registrador primário
+*/
+void asm_popcompare();
+
+/*
+*@brief altera registrador primário (e flags, indiretamente) conforme a comparação
+*/
+void asm_relop(char op);
+
+/*
+*@brief desvio incondicional
+*/
+void asm_jmp(int label);
+
+/*
+*@brief desvio se falso (0)
+*/
+void asm_jmpfalse(int label);
+
+/*
+*@brief lê um valor para o registrador primário e armazena em variável
+*/
+void asm_read();
+
+/*
+*@brief mostra valor do registrador primário
+*/
+void asm_write();
+
+/*
+*@brief inicia o compilador lendo o primeiro símbolo do programa de entrada
+*aloca espaço e inicializa a tabela de símbolos com 0
 */
 void init();
 
@@ -29,6 +131,10 @@ void fatal(char *s);
 */
 void expected(char *s);
 
+/*
+*@brief cria um rótulo
+*/
+int newLabel();
 
 /*
 *@brief verifica se a entrada dá match com o esperado, chamand expected c.c.
@@ -58,10 +164,153 @@ int isAddOp(char c);
 int isMulOp(char c);
 
 /*
-*@brief aloca espaço na memória para uma variável name e inicializa a mesma com getNum
-*caso o lookahead atual for "="
+*@brief reconhece um operador OU
+*/
+int isOrOp(char c);
+
+/*
+*@brief reconhece operadores relacionais.
+*/
+int isRelOp(char c);
+
+/*
+*@brief verifica se uma ariável existe na nossa tabela de símbolos
+*/
+int intable(char name);
+
+/*
+*@brief exibe uma mensagem  de erro relativa a referência a um identificador não declarado e
+*encerra a compilação
+*/
+void undefined(char name);
+
+/*
+*@brief verifica se a declaração analisada existe na tabela de símbolos, se existir retorna
+*uma mensagem de erro, c.c marca a variável na tabela de simbolos como declarada e aloca 
+*espaço na memória para a mesma
 */
 void allocVar(char name);
+
+/*
+*@brief verifica os tipos de declaração do código de entrada e desvia o fluxo do compilador
+*para os procedimento dos respectivos tipos detectados.
+*/
+void topDecls();
+
+/*
+*@brief verifica a declaração de uma ou mais variáveis
+*/
+void decl();
+
+/*
+*@brief
+*/
+void block();
+
+/*
+*@brief analisa e traduz um termo
+*/
+void term1();
+
+/*
+*@brief dispara as rotina para reconhecer e traduzir um termo
+*/
+void term();
+
+/*
+*@brief analisa e traduz um termo inicial
+*/
+void firstTerm();
+
+/*
+*@brief analisa e traduz uma expressão matemática
+*/
+void expression();
+
+/*
+*@brief analisa (é constante,variável ou '('??) e traduz um fator matemático
+*/
+void factor();
+
+/*
+*@brief analisa e traduz um fator negativo
+*/
+void negFactor();
+
+/*
+*@brief analisa e traduz um fator inicial
+*/
+void firstFactor();
+
+/*
+*@brief analisa e traduz um cmando de atribuição
+*/
+void assignment();
+
+/*
+*@brief analisa e traduz uma relação
+*/
+void relation();
+
+/*
+*@brief analisa e traduz um fator booleano com NOT inicial
+*/
+void notFactor();
+
+/*
+*@brief analisa e traduz um termo booleano
+*/
+void boolTerm();
+
+/*
+*@brief analisa e traduz uma expressão booleana
+*/
+void boolExpression();
+
+/*
+*@brief reconhece e traduz um "OR"
+*/
+void boolOr();
+
+/*
+*@brief reconhece e traduz um "xor"
+*/
+void boolXor();
+
+/*
+*@brief reconhece e dispara a rotina que traduz adição
+*/
+void add();
+
+/*
+*@brief reconhece e dispara a rotina que traduz subtração
+*/
+void subtract();
+
+/*
+*@brief reconhece e traduz uma multiplicação
+*/
+void multiply();
+
+/*
+*@brief reconhece e traduz divisão
+*/
+void divide();
+
+/*
+*@brief reconhece e traduz a estrutura de controle if
+*/
+void doIf();
+
+/*
+*@brief reconhece e traduz a estrutura de controle while
+*/
+void doWhile();
+
+/*
+*@brief verifica o bloco principal
+*/
+void mainBlock();
 
 /*
 *@brief reconhece os limitadores do programa de entrada escrito em Tiny
@@ -72,21 +321,6 @@ void prog();
 *@brief emite o código inicial necessário para o montador
 */
 void header();
-
-/*
-*@brief verifica os tipos de declaração do código de entrada e desvia o fluxo do compilador
-*para os procedimento dos respectivos tipos detectados.
-*/
-void topDecls();
-
-/*
-*/
-void decl();
-
-/*
-*@brief verifica o bloco principal
-*/
-void mainBlock();
 
 /*
 *@brief emite o código que identifica o programa principal para retornar ao sistema operacional
